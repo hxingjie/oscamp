efc8a94e5b7cd2a698ccedf11ae5e37288acef6e
hxingjie <wy15219110558@163.com>
2024-11-20 15:35:54 +0800

    lab1

--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -7,112 +7,53 @@ use allocator::{BaseAllocator, ByteAllocator, AllocResult, AllocError};
 use core::ptr::NonNull;
 use core::alloc::Layout;
 
+extern crate buddy_system_allocator;
+use buddy_system_allocator::Heap;
+
 extern crate log;
 use log::{debug, error, info, trace, warn};
 
-#[derive(Clone, Copy)]
-struct MemUnit {
-    ptr_byte: usize,
-    ptr_page: usize,
-    size: usize,
-}
-impl MemUnit {
-    const fn new() -> Self {
-        MemUnit { ptr_byte: 0, ptr_page: 0, size: 0 }
-    }
-}
-
 pub struct LabByteAllocator {
-    total_bytes: usize,
-    used_bytes: usize,
-
-    mem: [MemUnit; 8192],
-    mem_sz: usize,
+    inner: Heap<32>,
 }
 
 impl LabByteAllocator {
     pub const fn new() -> Self {
-        LabByteAllocator {
-            total_bytes: 0,
-            used_bytes: 0,
-
-            mem: [MemUnit::new(); 8192],
-            mem_sz: 0,
+        Self {
+            inner: Heap::<32>::new(),
         }
     }
 }
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        //unimplemented!();
-        warn!("Init EarlyAllocator, {}, {}", start, size);
-
-        self.mem[self.mem_sz].ptr_byte = start;
-        self.mem[self.mem_sz].size = size;
-
-        self.mem_sz += 1;
-
-        self.total_bytes = size;
-        
+        unsafe { self.inner.init(start, size) };
     }
+
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
-        warn!("Add Memory, {}, {}", start, size);
-        for i in 0..self.mem_sz {
-            if (start >= self.mem[i].ptr_byte && start <= self.mem[i].ptr_byte + self.mem[i].size-1)
-                || (start+size-1 >= self.mem[i].ptr_byte && start+size-1 <= self.mem[i].ptr_byte + self.mem[i].size-1) {
-                return Err(AllocError::MemoryOverlap);
-            }
-        }
-
-        self.mem[self.mem_sz].ptr_byte = start;
-        self.mem[self.mem_sz].size = size;
-
-        self.mem_sz += 1;
-
-        self.total_bytes += size;
-
+        unsafe { self.inner.add_to_heap(start, start + size) };
         Ok(())
     }
 }
 
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
-        let align = layout.align();
-        let size = layout.size();
-
-        warn!("alloc: {}, align: {}", size, align);
-
-        for i in 0..self.mem_sz {
-            let mut front_addr = self.mem[i].ptr_byte;
-            while front_addr % align != 0 { // align
-                front_addr += 1;
-            }
-            let back_addr = front_addr+size-1;
-            if back_addr < self.mem[i].ptr_page {
-                self.mem[i].ptr_byte += size;
-                self.used_bytes += size;
-                unsafe {
-                    warn!("total_bytes: {}, used_bytes: {}, available_bytes: {}", self.total_bytes(), self.used_bytes, self.available_bytes());
-                    return Ok(NonNull::new_unchecked(front_addr as *mut u8));
-                }
-            }
-        }
-        warn!("total_bytes: {}, used_bytes: {}, available_bytes: {}", self.total_bytes(), self.used_bytes(), self.available_bytes());
-        Err(AllocError::NoMemory)
+        self.inner.alloc(layout).map_err(|_| AllocError::NoMemory)
     }
+
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
-        //unimplemented!();
+        self.inner.dealloc(pos, layout)
     }
+
     fn total_bytes(&self) -> usize {
-        //unimplemented!();
-        self.total_bytes
+        self.inner.stats_total_bytes()
     }
+
     fn used_bytes(&self) -> usize {
-        //unimplemented!();
-        self.used_bytes
+        self.inner.stats_alloc_actual()
     }
+
     fn available_bytes(&self) -> usize {
-        //unimplemented!();
-        self.total_bytes - self.used_bytes
+        self.inner.stats_total_bytes() - self.inner.stats_alloc_actual()
     }
 }
